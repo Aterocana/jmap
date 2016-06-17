6d45b30005ca4bb829e8a55c01e4f7ed08b9e408
Aterocana <dominicimaurizio@gmail.com>
2016-06-17 17:06:47 +0200

    fix

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -3,32 +3,33 @@ var objectiveForm = document.querySelector('objective-form');
 var chatSidebar   = document.querySelector('chat-sidebar');
 var usersDiv      = document.getElementById('users');
 var socket        = io();
-var users         = {};
 
 socket.on('connect', function(){
+    /* imposto il socket a cui mandare i cambiamenti degli utenti*/
     loginForm.attachListener(function(){
         socket.emit('userChanged', loginForm.getUser());
     });
-
+    /* imposto il socket a cui mandare i nuovi messaggi*/
     chatSidebar.setSendMessage(function(data){
         socket.emit('msg', data);
     });
-
+    /* imposto il socket a cui mandare i cambiamenti di obiettivi*/
     objectiveForm.attachListener(function(){
         socket.emit('newObj', objectiveForm.getObjective());
     });
 
     socket.on('updateUsers', function(data){
-        users = data;
-        chatSidebar.setUsers(users);
-        console.log('updateUsers', users);
+        // inoltro gli utenti aggiornati
+        chatSidebar.setUsers(data);
+        console.log('updateUsers', data);
     });
 
-    socket.on('userChanged', function(data){
-        users[data.id] = data.user;
-        chatSidebar.setUsers(users);
-        console.log('userChanged', users);
-    });
+    // socket.on('userChanged', function(data){
+    //     // aggiungo il nuovo utente e inoltro gli utenti aggiornati
+    //     users[data.id] = data.user;
+    //     chatSidebar.setUsers(users);
+    //     console.log('userChanged', users);
+    // });
 
     socket.on('msg', function(data){
         console.log(data);
