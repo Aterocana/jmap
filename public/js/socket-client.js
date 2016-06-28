ded7b18d3d2d212afcaf992400867b882a739a64
Aterocana <dominicimaurizio@gmail.com>
2016-06-28 09:24:20 +0200

    branch closed

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,5 +1,4 @@
 var loginForm     = document.querySelector('login-form');
-// var objectiveForm = document.querySelector('objective-form');
 var chatSidebar   = document.querySelector('chat-sidebar');
 var mapDisplay    = document.querySelector('map-display');
 var socket        = io();
@@ -12,32 +11,6 @@ socket.on('connect', function(){
         chatSidebar.slideIn();
         mapDisplay.show();
     });
-    /* imposto il socket a cui mandare i nuovi messaggi*/
-    chatSidebar.setSendMessage(function(data){
-        socket.emit('msg', data);
-    });
-    /* imposto il socket a cui mandare i cambiamenti di obiettivi*/
-    // objectiveForm.attachListener(function(){
-    //     socket.emit('newObj', objectiveForm.getObjective());
-    // });
-
+    chatSidebar.setSocket(socket);
     mapDisplay.setSocket(socket);
-
-    socket.on('updateUsers', function(data){
-        // inoltro gli utenti aggiornati
-        chatSidebar.setUsers(data);
-        console.log('updateUsers', data);
-    });
-
-    // socket.on('userChanged', function(data){
-    //     // aggiungo il nuovo utente e inoltro gli utenti aggiornati
-    //     users[data.id] = data.user;
-    //     chatSidebar.setUsers(users);
-    //     console.log('userChanged', users);
-    // });
-
-    socket.on('msg', function(data){
-        console.log(data);
-        chatSidebar.setMessages(data);
-    });
 });
